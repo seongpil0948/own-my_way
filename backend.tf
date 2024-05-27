40dce36864398de2845289f6ab5df2d0ed54a2b1
최성필 <choeseongpil@choeseongpils-MacBook-Air.local>
2024-05-27 20:45:19 +0900

    fix:action

--- backend.tf
+++ backend.tf
@@ -33,9 +33,9 @@ terraform {
 
 
 provider "aws" {
-  region                = var.region
-  AWS_ACCESS_KEY_ID     = var.AWS_ACCESS_KEY_ID
-  AWS_SECRET_ACCESS_KEY = var.AWS_SECRET_ACCESS_KEY
+  region     = var.region
+  access_key = var.AWS_ACCESS_KEY_ID
+  secret_key = var.AWS_SECRET_ACCESS_KEY
   default_tags {
     tags = {
       Project = "aws-tfc",
